7c05e577be0c739290d64742cd75669a1b677b2d
Oleksandr Kryvonos <geraclion@gmail.com>
2018-10-20 00:35:07 +0300

    Added from template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NASATest2018.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
