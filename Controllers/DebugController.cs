f72c465fcfde71d0a183388cc4122a7593e11ee9
ServAlex <servalex1@gmail.com>
2018-10-20 19:30:31 +0300

    fixed userController typo, added debug method

---
+++ Controllers/DebugController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NASATest2018.Models;
+
+namespace NASATest2018.Controllers
+{
+    public class DebugController : Controller
+    {
+        [HttpPost]
+        public JsonResult GetUserList()
+        {
+            var result = new List<string>();
+
+            using(var context = new IsfContext())
+            {
+                foreach(var user in context.Users)
+                result.Add($"{user.UserId} - {user.Name} - {user.Phone}");
+            }
+            
+            return new JsonResult(result);
+        }
+    }
+}
