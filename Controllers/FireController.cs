8cb75832100adce0f0f750d24ea563ec4dc592f7
Oleksandr Kryvonos <geraclion@gmail.com>
2018-10-20 14:17:32 +0300

    Work in progress saveOfReport

--- Controllers/FireController.cs
+++ Controllers/FireController.cs
@@ -10,14 +10,45 @@ namespace NASATest2018.Controllers
 {
     public class FireController : Controller
     {
+        private bool checkSecretUserId(string SecretUserId)
+        {
+            return true;
+        }
 
         [HttpPost]
         public JsonResult ReportTheFire(ReportTheFireParameterDTO param)
         {
             var response = new ReportTheFireResponseDTO
             {
-                ReportId = 42
             };
+            //check secret user id
+            if(checkSecretUserId(param.SecretUserId))
+            {
+                using(var context = new IsfContext())
+                {
+                    var added = context.Reports.Add( 
+                        new Report
+                        {
+                            SecretUserId = param.SecretUserId,
+                            Longitude = param.Longitude,
+                            Latitude = param.Latitude,
+                            TextOfComment = param.TextOfComment,
+                            Timestamp = DateTime.UtcNow
+
+                        }
+
+                    );
+                    context.SaveChanges();
+                    response.ReportId = added.ReportId;
+                }
+                
+            }
+            else
+            {
+
+            }
+            
+            
             return new JsonResult(response);
         }
 
@@ -25,6 +56,7 @@ namespace NASATest2018.Controllers
         [HttpPost]
         public JsonResult GetSecretUserId()
         {
+            
             var response = new GetSecretUserIdResponseDTO
             {
                SecretUserId = "bla-bla-bla-42-secret-id"
