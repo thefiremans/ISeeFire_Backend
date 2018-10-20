22f59727ec0ce84bb1629bbd2f2d8c0c1bee1c77
ServAlex <servalex1@gmail.com>
2018-10-20 15:20:37 +0300

    fixed deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+echo 'pulling changes'
 git pull
+echo 'migrating'
+./applyMigration.sh
+echo 'removing deploy'
 rm -rf ./deploy
+echo 'running dotnet publish'
 dotnet publish -o ./deploy
