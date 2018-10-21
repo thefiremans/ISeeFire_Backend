2510db1827a4e84f6662d0b686a11d2ecbd46de2
Oleksandr Kryvonos <geraclion@gmail.com>
2018-10-21 09:39:03 +0300

    Added image path to migration

---
+++ Migrations/20181021063835_ExtendedReportWithImage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NASATest2018.Migrations
+{
+    public partial class ExtendedReportWithImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Reports",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Reports");
+        }
+    }
+}
