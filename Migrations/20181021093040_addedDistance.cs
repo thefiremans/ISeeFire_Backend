63b1cb265d8670582a31ce1ff55d1a469d3640b6
ServAlex <servalex1@gmail.com>
2018-10-21 12:32:38 +0300

    fixed distance column

---
+++ Migrations/20181021093040_addedDistance.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NASATest2018.Migrations
+{
+    public partial class addedDistance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Distance",
+                table: "Reports",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Distance",
+                table: "Reports");
+        }
+    }
+}
